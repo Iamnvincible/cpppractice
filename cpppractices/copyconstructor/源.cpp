148a9b77919863a0620840a4927f280f171c66bb
amvincible <ljlovemichael@hotmail.com>
2016-09-01 01:15:00 +0800

    copy constructor II

--- cpppractices/copyconstructor/源.cpp
+++ cpppractices/copyconstructor/源.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <string>
+#include <stdio.h>
 using namespace std;
 
 static int countobj = 0;
 class Obj {
 public:
+	char * name;
 	Obj() { countobj++; print("how many "); }
+	Obj(const char * i) { name = new char[::strlen(i) + 1]; ::strcpy(name, i); }
 	Obj(int i) { countobj++; }
-	Obj(const Obj& o) { countobj++; }
+	//Obj(const Obj& o) { countobj++; }
 	void print(const string& msg = "") {
 		if (msg.size() != 0)cout << msg << "";
 		cout << "countobj=" << countobj << endl;
@@ -15,6 +18,7 @@ public:
 	~Obj() {
 		countobj--;
 		print("~Obj()");
+		delete[] name;
 	}
 };
 Obj f(Obj o) {
@@ -23,6 +27,7 @@ Obj f(Obj o) {
 	cout << "end of f(Obj o)" << endl;
 	return o;
 }
+
 int main() {
 	Obj o;
 	o.print("after construction of o");
@@ -30,4 +35,11 @@ int main() {
 	Obj o2 = 10;
 	Obj o3 = f(o);
 	o.print("after call to f()");
+	char* a = "hello";
+	Obj o4 = "hello";
+	Obj o5 = o4;
+	printf("%p\n", o4.name);
+	printf("%p\n", o5.name);
+
+
 }
